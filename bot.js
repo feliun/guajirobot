133780dab56aafbb23bf5312a3c2d7331d743dba
feliun <felipe.polo@gmail.com>
2019-07-11 22:36:33 +0200

    Bot refactor. WIP

--- bot.js
+++ bot.js
@@ -4,89 +4,138 @@ const TelegramBot = require('node-telegram-bot-api');
 module.exports = ({ token }) => {
 	const bot = new TelegramBot(token, { polling: true });
 
-	const language = 'ES';
+	// const defaultLanguage = 'ES';
 
-	const setupVocabulary = cms => {
-		debug('Setting up vocabulary...');
-		bot.on('message', msg => {
-			const input = msg.text.toString().toLowerCase();
-			debug(`Looking up for input ${input} in language ${language}...`);
-			const match = cms.dictionary.lookup(language)(input);
-			if (match) {
-				bot.sendMessage(msg.chat.id, match);
-			}
-		});
-	};
 
-	const setupOnStart = () => {
-		bot.onText(/\/start/, msg => {
-			bot.sendMessage(msg.chat.id, 'Welcome to the V&F wedding bot! Bienvenido/a al bot de la boda de V&F');
-		});
-	};
+	const start = async ({ cms }) => {
+		// const languageCache = (database => {
+		// 	const languageByUserId = {};
 
-	const setupPictureSending = () => {
-		bot.onText(/\/sendpic/, msg => {
-			bot.sendPhoto(msg.chat.id, 'https://cdn0.bodas.net/usuarios/fotos/9/6/0/9/mfb_3469069.jpg?lu=1551276274', { caption: 'Besitos de la pareja! \nEstamos deseando verte' });
-		});
-	};
+		// 	const updateUser = async ({ user, language }) => {
+		// 		await database.updateProfile({ ...user, language });
+		// 		languageByUserId[user.id] = language;
+		// 	};
 
-	const setupVenueQuery = () => {
-		bot.onText(/\/venue/, msg => {
-			bot.sendLocation(msg.chat.id, 44.97108, -104.27719);
-			bot.sendMessage(msg.chat.id, 'Here is the point');
-		});
-	};
+		// 	const getLanguageByUserId = userId => languageByUserId[userId] || defaultLanguage;
 
-	const setupTrivia = () => {
-		bot.on('callback_query', msg => {
-			console.log(JSON.stringify(msg));
-		});
+		// 	return { updateUser, getLanguageByUserId };
+		// })(db);
 
-		const questions = [
-			{
-				id: 1,
-				question: 'When did we kiss for the first time?',
-				answers: [
-					{
-						text: 'London',
-						hit: false,
-					},
-					{
-						text: 'Madrid',
-						hit: false,
-					},
-					{
-						text: 'In the middle of a flight',
-						hit: true,
-					},
-				],
-			},
-		];
+		const setupVocabulary = () => {
+			debug('Setting up vocabulary...');
+			bot.on('message', msg => {
+				const input = msg.text.toString().toLowerCase();
+				const userData = msg.from;
+				const language = userData.language_code.toUpperCase();
+				debug(`Looking up for input ${input} in language ${language}...`);
+				const match = cms.dictionary.lookup(language)(input);
+				if (match) {
+					bot.sendMessage(msg.chat.id, match);
+				}
+			});
+		};
 
-		bot.onText(/\/trivia/, msg => {
-			bot.sendMessage(msg.chat.id, questions[0].question, {
+		const languageQuery = chatId => {
+			bot.on('callback_query', async msg => {
+				console.log(JSON.stringify(msg));
+				// const language = 'EN';
+				// const user = {};
+				// await languageCache.updateUser({ ...user, language });
+			});
+
+			bot.sendMessage(chatId, 'Elige tu idioma / Please choose your language', {
 				reply_markup: {
 					inline_keyboard: [
 						[{
-							text: questions[0].answers[0].text,
-							callback_data: 'question:1,answer:0',
-						}, {
-							text: questions[0].answers[1].text,
-							callback_data: 'question:1,answer:1',
-						}, {
-							text: questions[0].answers[2].text,
-							callback_data: 'question:1,answer:2',
-						},
-						]],
+							text: 'Español',
+							callback_data: 'language:ES',
+						}],
+						[{
+							text: 'English',
+							callback_data: 'language:EN',
+						}],
+					],
 				},
 			});
-		});
-	};
+		};
 
-	const start = async ({ cms }) => {
+		const setupLanguage = () => {
+			bot.onText(/\/language/, msg => {
+				languageQuery(msg.chat.id);
+			});
+		};
+
+		const setupOnStart = () => {
+			bot.onText(/\/start/, msg => {
+				console.log(JSON.stringify(msg));
+				// bot.sendMessage(msg.chat.id, 'Welcome to the V&F wedding bot! Bienvenido/a al bot de la boda de V&F');
+				// languageQuery(msg.chat.id);
+			});
+		};
+
+		const setupPictureSending = () => {
+			bot.onText(/\/sendpic/, msg => {
+				bot.sendPhoto(msg.chat.id, 'https://cdn0.bodas.net/usuarios/fotos/9/6/0/9/mfb_3469069.jpg?lu=1551276274', { caption: 'Besitos de la pareja! \nEstamos deseando verte' });
+			});
+		};
+
+		const setupVenueQuery = () => {
+			bot.onText(/\/venue/, msg => {
+				bot.sendLocation(msg.chat.id, 44.97108, -104.27719);
+				bot.sendMessage(msg.chat.id, 'Here is the point');
+			});
+		};
+
+		const setupTrivia = () => {
+			// bot.on('callback_query', msg => {
+			//   console.log(JSON.stringify(msg));
+			// });
+
+			const questions = [
+				{
+					id: 1,
+					question: 'When did we kiss for the first time?',
+					answers: [
+						{
+							text: 'London',
+							hit: false,
+						},
+						{
+							text: 'Madrid',
+							hit: false,
+						},
+						{
+							text: 'In the middle of a flight',
+							hit: true,
+						},
+					],
+				},
+			];
+
+			bot.onText(/\/trivia/, msg => {
+				bot.sendMessage(msg.chat.id, questions[0].question, {
+					reply_markup: {
+						inline_keyboard: [
+							[{
+								text: questions[0].answers[0].text,
+								callback_data: 'question:1,answer:0',
+							}, {
+								text: questions[0].answers[1].text,
+								callback_data: 'question:1,answer:1',
+							}, {
+								text: questions[0].answers[2].text,
+								callback_data: 'question:1,answer:2',
+							},
+							]],
+					},
+				});
+			});
+		};
+
 		console.log('Configuring bot....');
-		setupVocabulary(cms);
+		setupVocabulary();
 		setupOnStart();
+		setupLanguage();
 		setupPictureSending();
 		setupVenueQuery();
 		setupTrivia();

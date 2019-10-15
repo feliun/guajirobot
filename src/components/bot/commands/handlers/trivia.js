53ee1b5d921a6acae6e5b4bd45d38735c9c92e0c
feliun <felipe.polo@gmail.com>
2019-10-15 17:58:07 +0200

    Vertical format for trivia choices

--- src/components/bot/commands/handlers/trivia.js
+++ src/components/bot/commands/handlers/trivia.js
@@ -4,10 +4,10 @@ module.exports = (controller, bot) => async msg => {
 	const format = (question, winner) => answers => answers.map((answer, index) => {
 		// https://github.com/nickoala/telepot/issues/293#issuecomment-326426090
 		const response = `command=trivia,q=${question},a=${index + 1},win=${winner === (index + 1)}`;
-		return {
+		return [{
 			text: answer.text,
 			callback_data: response,
-		};
+		}];
 	});
 
 	const user = msg.from;
@@ -17,7 +17,7 @@ module.exports = (controller, bot) => async msg => {
 		await bot.sendMessage(msg.chat.id, 'You completed all the trivia questions!');
 		return;
 	}
-	const choices = [format(question.question, question.winner)(question.answers)];
+	const choices = format(question.question, question.winner)(question.answers);
 	await bot.sendMessage(msg.chat.id, question.text, {
 		reply_markup: {
 			inline_keyboard: choices,

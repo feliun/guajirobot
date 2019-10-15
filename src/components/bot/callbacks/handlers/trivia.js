222fae262bfc5092c7175cd61508bdcc60f0ca1d
feliun <felipe.polo@gmail.com>
2019-10-15 17:47:03 +0200

    Fix when last name is not provided

--- src/components/bot/callbacks/handlers/trivia.js
+++ src/components/bot/callbacks/handlers/trivia.js
@@ -1,6 +1,12 @@
 const debug = require('debug')('guajirobot:bot:callbacks:handler:trivia');
 
 module.exports = (controller, bot) => async msg => {
+
+	const composeName = user => {
+		if (!user.last_name) return `${user.first_name}`;
+		return `${user.first_name} ${user.last_name}`;
+	};
+
 	const user = msg.from;
 	const questionId = parseInt(msg.data.q, 10);
 	const answer = parseInt(msg.data.a, 10);
@@ -8,6 +14,6 @@ module.exports = (controller, bot) => async msg => {
 	const chatId = msg.message.chat.id;
 	debug(`Processing answer for question ${questionId}...`);
 	await controller(user).registerTriviaAnswer(questionId, answer, won);
-	await bot.sendMessage(chatId, `Thanks ${user.first_name} ${user.last_name}! 📝`);
+	await bot.sendMessage(chatId, `Thanks ${composeName(user)}! 📝`);
 	return Promise.resolve();
 };
